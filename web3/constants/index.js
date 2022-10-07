2ea2769ff576f7d7cab9329d2a37981baf23d388
Vaibhav Sharma <vaibhav@Vaibhavs-MacBook-Air.local>
2022-10-07 15:15:29 +0530

    updated the endpoint in hardhat constants

--- web3/constants/index.js
+++ web3/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xC9212f423291D1895dF65e7759B1684c36A24058"
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-iota-black.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
